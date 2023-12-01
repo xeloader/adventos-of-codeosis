c250f072180d766de1c1f99243506edc783c20d0
Victor Ingman <viregistermail@gmail.com>
2023-12-01 20:14:21 +0100

    :sparkles: 2023-01-p1

--- packages/2023/src/01/index.ts
+++ packages/2023/src/01/index.ts
@@ -1,3 +1,47 @@
-export const main = () => {
-  console.log('Hello I am 1')
+import fs from 'fs'
+
+const isNumber = (character: string) => character <= '9' && character >= '0'
+
+const calibrationValue = (line: string): [number, number] => {
+  let ptrA = 0
+  let ptrB = line.length - 1
+  let a: number | undefined
+  let b: number | undefined
+  while (
+    (a == null || b == null) &&
+    ptrA <= line.length &&
+    ptrB >= 0
+  ) {
+    
+    if (a == null) {
+      const valA = line[ptrA]
+      if (isNumber(valA)){
+        a = Number(valA)
+      } else {
+        ptrA += 1
+      }
+    }
+    if (b == null) {
+      const valB = line[ptrB]
+      if (isNumber(valB)) {
+        b = Number(valB)
+      } else {
+        ptrB -= 1
+      }
+    }
+  }
+  return [a, b] as [number, number]
 }
+
+export const main = () => {
+  const exampleInput = fs.readFileSync('./data/01/full.txt').toString()
+  const result = exampleInput
+    .split('\n')
+    .map(calibrationValue)
+    .map((cns) => cns.join(''))
+    .map(Number)
+    .reduce((acc, cur) => acc + cur, 0)
+  console.log(`Result is: ${result}`)
+}
+
+
